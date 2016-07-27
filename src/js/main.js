0227cc2f1212b1b39f9d1dcce5aded65075236f0
Sergey Shuyskiy <s.shuyskiy@qiwi.ru>
2016-07-27 20:12:26 +0300

    added chart for viewers

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,54 @@
-// var data;
+// var chartMaxCounter = 0;
+var startingChartData = [0, 0, 0, 0, 0, 0, 0];
+var canvas = document.getElementById('viewers_chart'),
+    ctx = canvas.getContext('2d'),
+    startingChartParams = {
+      labels: [1, 2, 3, 4, 5, 6, 7],
+      datasets: [
+          {
+              fillColor: "rgba(220,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              data: startingChartData
+          }
+      ]
+    },
+    latestLabel = startingChartParams.labels[6];
+
+var myLiveChart = new Chart.Line(ctx, {data: startingChartParams});
+
 //function ready() {
-ajax();
-setInterval(ajax, 15000);
+getStreamInfo();
+setInterval(getStreamInfo, 15000);
 //};
 
 //document.addEventListener("DOMContentLoaded", ready);
 
-function req() {
-  console.log('tost');
-}
-
-function ajax() {
-  var data;
+function getStreamInfo() {
   var stream = 'anaxuname';
   var link = "https://streams.twitch.tv/kraken/streams?channel=" + stream + "&on_site=1";
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      data = JSON.parse(xhttp.responseText);
-      if (data && data._total === 0) {
-        document.getElementById("info").hidden = false;
-        document.getElementsByClassName("content")[0].hidden = true;
-      } else if (data) {
-        updateInfo(data);
-      };
-    };
-  };
-  xhttp.open("GET", link, true);
-  xhttp.send();
+  ajax(link,
+    function (data) {
+      if (data._total != 0) {
+        foundStream(data);
+      } else {
+        noStream();
+      }
+    },
+    noStream
+  );
 }
 
-function updateInfo(data) {
+function noStream() {
+  document.getElementById("info").hidden = false;
+  document.getElementsByClassName("content")[0].hidden = true;
+}
+
+function foundStream(data) {
+  startingChartData.push(data.streams[0].viewers);
+  startingChartData.shift();
+  myLiveChart.update();
   document.getElementById("info").hidden = true;
   document.getElementsByClassName("content")[0].hidden = false;
   document.getElementById("viewers").innerHTML = data.streams[0].viewers;
@@ -40,19 +58,20 @@ function updateInfo(data) {
 }
 
 
-function request(link) {
-  var result;
+function ajax(url, success = function () {}, fail = function () {}) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      result = JSON.parse(xhttp.responseText);
-    };
-  };
-  xhttp.open(
-    "GET",
-    link,
-    true
-  );
+      var data = JSON.parse(xhttp.responseText);
+      if (data) {
+        success(data);
+      } else {
+        fail();
+      }
+    } else {
+      fail();
+    }
+  }
+  xhttp.open("GET", url, true);
   xhttp.send();
-  return result;
 }
