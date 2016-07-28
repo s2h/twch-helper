263b0a6a3195e440918d72d2e1e6b0c9f366dc61
Sergey Shuyskiy <s.shuyskiy@qiwi.ru>
2016-07-28 14:52:39 +0300

    redisign chart a bit, fixed some bugs, reformatting code

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
-// var chartMaxCounter = 0;
-var startingChartData = [0, 0, 0, 0, 0, 0, 0];
+var chartColumnsNum = 40,
+    getInfoTimeout = 60000,
+    stream = 'anaxuname';
+
+var startingChartData,
+    startingChartLabels,
+    myLiveChart;
+
+fillStartingDataForCharts();
+
 var canvas = document.getElementById('viewers_chart'),
     ctx = canvas.getContext('2d'),
     startingChartParams = {
-      labels: [1, 2, 3, 4, 5, 6, 7],
+      labels: startingChartLabels,
       datasets: [
           {
+              label: "viewers",
               fillColor: "rgba(220,220,220,0.2)",
               strokeColor: "rgba(220,220,220,1)",
               pointColor: "rgba(220,220,220,1)",
@@ -13,20 +22,22 @@ var canvas = document.getElementById('viewers_chart'),
               data: startingChartData
           }
       ]
-    },
-    latestLabel = startingChartParams.labels[6];
+    };
 
-var myLiveChart = new Chart.Line(ctx, {data: startingChartParams});
-
-//function ready() {
+setInterval(getStreamInfo, getInfoTimeout);
+myLiveChart = new Chart.Line(ctx, {data: startingChartParams});
 getStreamInfo();
-setInterval(getStreamInfo, 15000);
-//};
 
-//document.addEventListener("DOMContentLoaded", ready);
+function fillStartingDataForCharts() {
+  startingChartData = [];
+  startingChartLabels = [];
+  for (var i = 0; i < chartColumnsNum; i++ ) {
+    startingChartData.push(0);
+    startingChartLabels.push("");
+  }
+}
 
 function getStreamInfo() {
-  var stream = 'anaxuname';
   var link = "https://streams.twitch.tv/kraken/streams?channel=" + stream + "&on_site=1";
   ajax(link,
     function (data) {
@@ -57,7 +68,6 @@ function foundStream(data) {
   document.getElementById("status").innerHTML = data.streams[0].channel.status;
 }
 
-
 function ajax(url, success = function () {}, fail = function () {}) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -65,12 +75,10 @@ function ajax(url, success = function () {}, fail = function () {}) {
       var data = JSON.parse(xhttp.responseText);
       if (data) {
         success(data);
-      } else {
-        fail();
+        return
       }
-    } else {
-      fail();
     }
+    fail();
   }
   xhttp.open("GET", url, true);
   xhttp.send();
